1a494a6976606adce6e093f2a08101da2db3aa08
sara hussien <sarahussienelhussieny@gmail.com>
2026-05-22 16:30:29 +0300

    update routes ,add main layout and auth layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Layout from "./components/layout/Layout";
+import MainLayout from "./components/layout/MainLayout";
+import AuthLayout from "./components/layout/AuthLayout";
 import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import BlogsPage from "./pages/BlogsPage/BlogsPage";
@@ -9,28 +10,35 @@ import PlacesPage from "./pages/PlacesPage/PlacesPage";
 import LoginPage from './features/auth/pages/LoginPage';
 import UserRegisterPage from './pages/UserRegisterPage';
 import RoleSelectionPage from './features/auth/pages/RoleSelection';
-import { ForgotPassword, OTPVerification, ResetPassword ,RoleSelection} from './features/auth';
+import { ForgotPassword, OTPVerification, ResetPassword } from './features/auth';
 import { DriverRegisterAuth } from './features/auth';
-import { DriverForm }      from './features/driver';
+import { DriverForm } from './features/driver';
 import "./App.css";
 import RegisterPlace from "./pages/RegisterPlacePage";
+
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="roleselection" element={<RoleSelectionPage />} />
+      {/* صفحات مع Navbar و Footer */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
         <Route path="blogs" element={<BlogsPage />} />
+        <Route path="places" element={<PlacesPage />} />   {/* ملاحظة: كان موجوداً في Route السابق */}
+       
+        <Route path="driver-form" element={<DriverForm />} />
+      </Route>
+
+      {/* صفحات بدون Navbar و Footer (صفحات Auth) */}
+      <Route element={<AuthLayout />}>
         <Route path="login" element={<LoginPage />} />
         <Route path="user" element={<UserRegisterPage />} />
+        <Route path="roleselection" element={<RoleSelectionPage />} />
         <Route path="forgetPassword" element={<ForgotPassword />} />
         <Route path="otp" element={<OTPVerification />} />
         <Route path="reset-password" element={<ResetPassword />} />
-        <Route path="driver-form" element={<DriverForm />} />
+         <Route path="register-place" element={<RegisterPlace />} />
         <Route path="driver" element={<DriverRegisterAuth />} />
-        <Route path="register-place" element={<RegisterPlace />} />
-
       </Route>
     </Routes>
   );
