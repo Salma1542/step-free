211e0bc4fec54af4d909a84dc0fba866c628be59
Salma1542 <salmaabdelwahed2003@gmail.com>
2026-06-10 14:13:23 +0300

    Add updates

--- src/App.jsx
+++ src/App.jsx
@@ -46,13 +46,13 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 export default function App() {
-  // useEffect(() => {
-  //   AOS.init({
-  //     duration: 1000,
-  //     once: true,
-  //     easing: "ease-in-out",
-  //   });
-  // }, []);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      easing: "ease-in-out",
+    });
+  }, []);
 
   return(
     <>
