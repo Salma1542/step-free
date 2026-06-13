0cbbd333adf531468c45f95d7c2d409b513d0cad
mennatallah <mennatallah@gmail.com>
2026-06-13 11:27:30 +0300

    save reviewssection  changes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-
 import Layout from "../components/layout/Layout";
 import AboutPage from "../pages/AboutPage";
 import PlacesPage from "../pages/PlacesPage/PlacesPage";
 import BlogsPage from "../pages/BlogsPage/BlogsPage";
-import ExplorePage from "./pages/ExplorePage";
+import ExplorePage from "../pages/ExplorePage";  
+
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="about" element={<AboutPage />} />
-        <Route path="about" element={<PlacesPage />} />
+        <Route path="place/:id" element={<PlacesPage />} />  
         <Route path="blogs" element={<BlogsPage />} />
         <Route path="explore" element={<ExplorePage />} />
       </Route>
